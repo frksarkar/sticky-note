01753e26e907d33f4e2b7ec17eae64e6521e8acd
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-06-22 23:27:19 +0600

    add element function

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
+const body = document.querySelector('#app');
+const addBtn = document.querySelector('.add-note');
 
-	burger.addEventListener('click', () => {
-		// Toggle Nav
-		nav.classList.toggle('nav-active');
+addBtn.addEventListener('click', () => {
+	const element = `<textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        class="note"
+                        placeholder="Write something new..."
+                    ></textarea>`;
 
-		// Animate Links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${
-					index / 7 + 0.3
-				}s`;
-			}
-		});
-
-		// Burger Animation
-		burger.classList.toggle('toggle');
-	});
-};
-
-navSlide();
+	body.insertAdjacentHTML('beforeend', element);
+});
